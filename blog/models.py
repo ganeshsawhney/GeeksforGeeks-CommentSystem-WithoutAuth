d2815d6a493a93ee490c6af4a10cdbe58d02d841
Ganesh Sawhney <ganeshsawhney@gmail.com>
2016-04-28 19:17:06 +0530

    Added Moderation

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.core.urlresolvers import reverse
 
 
 class Post(models.Model):
@@ -18,6 +19,5 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
-
-
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk':self.id})
