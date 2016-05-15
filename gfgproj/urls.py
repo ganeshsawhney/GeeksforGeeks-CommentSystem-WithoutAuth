14373922d1b0b0cb4076b0b976f966b023d45d31
Ganesh Sawhney <ganeshsawhney@gmail.com>
2016-05-15 20:58:10 +0530

    Multiple Preview Bug removed

--- gfgproj/urls.py
+++ gfgproj/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('blog.urls')),
     url(r'^post/comments/', include('fluent_comments.urls')),
+    url(r'^ratings/', include('ratings.urls')),
 ]
